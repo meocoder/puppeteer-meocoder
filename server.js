57a26401142b619354dbacfdb6bbf03e596a7a74
Jan Keromnes <janx@linux.com>
2020-09-02 14:45:14 +0000

    Add Puppeteer test project

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const puppeteer = require('puppeteer')
+
+const app = express();
+
+const port = process.env.port || 5000;
+
+app.get('/', (req,res)=>{
+    res.send();
+})
+
+app.listen(port, ()=> {
+    console.log("port running ");
+});
+
+(async function () {
+  const browser = await puppeteer.launch({ headless: true}); // We use this option to go into non-headless mode
+  const page = await browser.newPage(); // Create a new page instance
+  await page.goto("https://pptr.dev"); // Navigate to the pptr.dev website
+
+  await page.waitFor(5000); // Wait for 5 seconds to see the beautiful site
+  await browser.close(); // Close the browser
+})();
